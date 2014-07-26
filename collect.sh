0eb3f7203f699c8a2e613a57d6400dfc1a49f6de
Celso Martins <celso.av.martins@gmail.com>
2014-07-26 16:39:50 -0300

    melhoria no layout e acerto na rake task

---
+++ collect.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export MONGOHQ_URL=mongodb://heroku:nJVg7oryTMOLVV_Xlg3O5l1S_nU6AiIQM1aPr-5d_gIe04LBaxIy8zlunsjXfcL2pWmcs7_BHG2b0JvZJx6o2g@kahana.mongohq.com:10074/app27785650
+/home/celso/.rbenv/shims/rake RAILS_ENV=production data_collect:collect
